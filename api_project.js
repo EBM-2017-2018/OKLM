e214bc96755b66160986cf8c70d12dee88edcfa9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-07 09:00:57 +0000

    Deploy EBM-2017-2018/OKLM to github.com/EBM-2017-2018/OKLM.git:gh-pages

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "ApiDoc pour le projet Boilerplate",
+  "url": "/api",
+  "sampleUrl": "http://localhost:4000/api",
+  "name": "boilerplate-backend",
+  "version": "0.1.0",
+  "description": "",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-02-07T09:00:41.123Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
