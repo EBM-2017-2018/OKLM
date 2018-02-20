509ef9d2496e12ce2c43dcc84fb4a4dca649b7df
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2018-02-20 20:29:57 +0100

    First implementation of Material

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Button, Reboot} from 'material-ui';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Reboot/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Button variant="raised">
+            Click me!
+          </Button>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
