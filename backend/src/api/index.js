993d1accf9c4b9abe16d262c16303eea300e9c55
Ekar <antoine.robillard@noos.fr>
2018-03-07 16:22:24 +0100

    add model directory in api for resources

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 
 const router = new Router();
 
-router.use('/documents', require('./documents'));
-router.use('/categories', require('./categories'));
-router.use('/users', require('./users'));
+router.use('/documents', require('./model/documents'));
+router.use('/categories', require('./model/categories'));
+router.use('/users', require('./model/users'));
+
 
 module.exports = router;
