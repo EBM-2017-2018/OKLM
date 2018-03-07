8be9288fb9cc17da16f1730081f28de3bde99026
Ekar <antoine.robillard@noos.fr>
2018-03-07 16:31:58 +0100

    rename model directory as resources

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -2,9 +2,9 @@ const { Router } = require('express');
 
 const router = new Router();
 
-router.use('/documents', require('./model/documents'));
-router.use('/categories', require('./model/categories'));
-router.use('/users', require('./model/users'));
+router.use('/documents', require('./resources/documents'));
+router.use('/categories', require('./resources/categories'));
+router.use('/users', require('./resources/users'));
 
 
 module.exports = router;
