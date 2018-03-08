9b3fe1ba326f2ab6925b2985cf04d3d59a0eeccd
Hazegard <m.catrice@outlook.com>
2018-03-08 13:58:20 +0100

    feature:add scoring and sorting / refact:extract config

--- backend/src/api/search/search.controller.js
+++ backend/src/api/search/search.controller.js
@@ -1,61 +1,60 @@
 const Document = require('../resources/documents/document.model');
 const Category = require('../resources/categories/category.model');
 
+const { textSearch, score } = require('./search.config');
+
 module.exports = {};
-const searchInDocuments = fields => Document.find({
-  $text: {
-    $search: fields,
-    $diacriticSensitive: true,
-  },
-});
+
+const queryInDocument = fields => Category.find(textSearch(fields), score)
+  .sort(score);
+const queryInCategory = fields => Document.find(textSearch(fields), score)
+  .sort(score);
 
 
-const searchInCategories = fields => Category.find({
-  $text: {
-    $search: fields,
-    $diacriticSensitive: true,
-  },
-});
+const searchInDocument = (res, fields) => {
+  queryInDocument(fields)
+    .then(docs => res.status(200)
+      .json(docs))
+    .catch(err => res.status(500)
+      .json(err));
+};
 
+const searchInCategory = (res, fields) => {
+  queryInCategory(fields)
+    .then(cat => res.status(200)
+      .json(cat))
+    .catch(err => res.status(500)
+      .json(err));
+};
+
+const searchInBoth = (res, fields) => {
+  Promise.all([
+    queryInCategory(fields),
+    queryInDocument(fields),
+  ])
+    .then(([cats, docs]) => ({
+      categories: cats,
+      documents: docs,
+    }))
+    .then(content => res.status(200)
+      .json(content))
+    .catch(err => res.status(500)
+      .json(err));
+};
 module.exports.search = (req, res) => {
   let where;
   if (Array.isArray(req.query.content)) {
     where = req.query.content;
-  } else {
+  } else if (req.query.content) {
     where = req.query.content.split(',');
   }
   const doQueryInDocuments = req.query.content ? where.includes('document') : true;
   const doQueryInCategories = req.query.content ? where.includes('category') : true;
   if (doQueryInCategories && doQueryInDocuments) {
-    Promise.all([
-      searchInCategories(req.query.q),
-      searchInDocuments(req.query.q),
-    ])
-      .then(([cats, docs]) => ({
-        categories: cats,
-        documents: docs,
-      }))
-      .then(content => res.status(200)
-        .json(content))
-      .catch(err => res.status(500)
-        .json(err));
+    searchInBoth(res, req.query.q);
   } else if (doQueryInCategories) {
-    searchInCategories(req.query.q)
-      .then((cat) => {
-        res.status(200)
-          .json(cat);
-      })
-      .catch(err => res.status(500)
-        .json(err));
+    searchInCategory(res, req.query.q);
   } else if (doQueryInDocuments) {
-    searchInDocuments(req.query.q)
-      .then((docs) => {
-        res.status(200)
-          .json(docs);
-      })
-      .catch((err) => {
-        res.status(500)
-          .json(err);
-      });
+    searchInDocument(res, req.query.q);
   }
 };
