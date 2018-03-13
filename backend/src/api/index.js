016a07853c5e0e9c937bde476262bca9987797f1
Ekar <antoine.robillard@noos.fr>
2018-03-13 09:37:46 +0100

    add download route

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -5,6 +5,6 @@ const router = new Router();
 router.use('/documents', require('./resources/documents'));
 router.use('/categories', require('./resources/categories'));
 router.use('/users', require('./resources/users'));
-
+router.use('/download', require('./download'));
 
 module.exports = router;
