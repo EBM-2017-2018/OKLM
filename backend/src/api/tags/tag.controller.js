4a7d1c8380a9379f476c349a2abf447f761fa939
Hazegard <m.catrice@outlook.com>
2018-03-14 17:17:59 +0100

    migrate from callback to promise

--- backend/src/api/tags/tag.controller.js
+++ backend/src/api/tags/tag.controller.js
@@ -2,41 +2,42 @@ const Tag = require('./tag.model');
 
 module.exports = {};
 
+const getAll = () => Tag.find({});
+const getTagById = id => Tag.findOne({ _id: id });
+
 module.exports.getAll = (req, res) => {
-  Tag.find({}, (err, tags) => {
-    if (err) {
-      return res.status(500).json(err);
-    }
-    return res.json(tags);
-  });
+  getAll()
+    .then(tags => res.status(200)
+      .json(tags))
+    .catch(err => res.status(500)
+      .json(err));
 };
 
 module.exports.create = (req, res) => {
   const tag = new Tag(req.body);
   tag.save((err) => {
     if (err) {
-      return res.status(500).json(err);
+      return res.status(500)
+        .json(err);
     }
-    return res.status(201).json(tag);
+    return res.status(201)
+      .json(tag);
   });
 };
 
 module.exports.getOne = (req, res) => {
-  Tag.findOne(
-    { _id: req.params.id },
-    (err, tag) => {
-      if (err) {
-        return res.status(500).json(err);
-      }
+  getTagById(req.params.id)
+    .then((tag) => {
       if (!tag) {
-        return res.status(404).json({
-          code: 'TAG_NOT_FOUND',
-          message: 'L\'utilisateur n\'a pas pu être trouvé',
-        });
+        return res.status(404)
+          .json({
+            code: 'TAG_NOT_FOUND',
+            message: 'L\'utilisateur n\'a pas pu être trouvé',
+          });
       }
-      return res.status(200).json(tag);
-    },
-  );
+      return res.status(200)
+        .json(tag);
+    });
 };
 
 module.exports.delete = (req, res) => {
@@ -44,9 +45,11 @@ module.exports.delete = (req, res) => {
     { _id: req.params.id },
     (err) => {
       if (err) {
-        return res.status(500).json(err);
+        return res.status(500)
+          .json(err);
       }
-      return res.status(204).end();
+      return res.status(204)
+        .end();
     },
   );
 };
