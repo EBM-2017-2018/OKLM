01dd54587a545c038409546bdf76629f415ccb24
Maxime Catrice <m.catrice@outlook.com>
2018-03-19 10:31:11 +0100

    add sorting tests

--- backend/src/api/search/search.config.js
+++ backend/src/api/search/search.config.js
@@ -1,9 +1,14 @@
 module.exports = {};
 
-module.exports.score = {
+module.exports.sort = {
   score: {
-    $meta: 'textScore',
+    score: {
+      $meta: 'textScore',
+    },
   },
+  date_asc: ['creationTime', 1],
+  date_desc: ['creationTime', -1],
+  rank: ['$score', -1],
 };
 module.exports.textSearch = fields => ({
   $text: {
@@ -12,3 +17,8 @@ module.exports.textSearch = fields => ({
     $diacriticSensitive: false,
   },
 });
+module.exports.score = {
+  score: {
+    $meta: 'textScore',
+  },
+};
--- backend/src/api/search/search.controller.js
+++ backend/src/api/search/search.controller.js
@@ -2,20 +2,45 @@ const Document = require('../resources/documents/document.model');
 const Category = require('../resources/categories/category.model');
 
 const { addAuthorsToListDocuments } = require('../resources/documents/documents.controller');
-const { textSearch, score } = require('./search.config');
+const { textSearch, sort, score } = require('./search.config');
 
 module.exports = {};
 
-const queryInDocument = fields => Document.find(textSearch(fields), score)
-  .sort(score)
-  .then(docs => addAuthorsToListDocuments(docs));
+const getSortType = (sortType) => {
+  if (sortType === 'date_desc') {
+    return [sort.date_desc];
+  } else if (sortType === 'date_asc') {
+    return [sort.date_asc];
+  } else if (sortType === 'rank') {
+    return [sort.rank];
+  }
+  return [[]];
+  // console.log(sortType);
+  // const sortBy = sortType.split(',');
+  // return sortBy.reduce((prev, curr) => {
+  //   if (curr === 'date_desc') {
+  //     prev.push(['creationTime', -1]);
+  //   } else if (curr === 'rank') {
+  //     prev.push(['$score', -1]);
+  //   }
+  //   return prev;
+  // }, []);
+};
+
+const queryInDocument = (fields, sortType) => {
+  const sortBy = getSortType(sortType);
+  return Document.find(textSearch(fields), score)
+    .sort(sortBy)
+    .then(docs => addAuthorsToListDocuments(docs));
+};
 
 const queryInCategory = fields => Category.find(textSearch(fields), score)
   .sort(score);
 
 const getContentTypes = (query) => {
-  if (Array.isArray(query)) return query;
-  else if (query) return query.split(',');
+  if (Array.isArray(query)) {
+    return query;
+  } else if (query) return query.split(',');
 
   return ['documents', 'categories'];
 };
@@ -23,6 +48,7 @@ const getContentTypes = (query) => {
 module.exports.search = async (req, res) => {
   const types = getContentTypes(req.query.type);
 
+  const sorted = req.query.sort;
   const results = {};
 
   if (types.includes('categories')) {
@@ -30,7 +56,7 @@ module.exports.search = async (req, res) => {
   }
 
   if (types.includes('documents')) {
-    results.documents = await queryInDocument(req.query.q);
+    results.documents = await queryInDocument(req.query.q, sorted);
   }
 
   res.send(results);
