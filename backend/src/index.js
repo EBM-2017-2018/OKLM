243c001757cf7f5cf70c0e7d1c9cafef028eb651
Thomas Gaudin <t.goudine@gmail.com>
2018-03-19 10:46:54 +0100

    Fix serveStatic

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use('/api', require('./api'));
 app.use(serveStatic('./public'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 server.listen(config.app.port, (err) => {
