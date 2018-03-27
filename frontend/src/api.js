a26018567845a7d07436ae0cdf8be332e019e38b
Clément Michel <m1ch3lcl@gmail.com>
2018-03-27 17:03:27 +0200

    add my docs endpoint and refact api service

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -9,6 +9,11 @@ const handleHttpErrors = response => {
   return response;
 };
 
+const apiFetch = (url, options = {}) => fetch(BASE_URL + url, {
+  ...options,
+  headers: getAuthHeaders(options.headers)
+}).then(checkAuthResponse).then(handleHttpErrors)
+
 export const createDoc = ({ title, uri, file, motherCategory }) => {
   if (file) {
     const fd = new FormData();
@@ -16,45 +21,42 @@ export const createDoc = ({ title, uri, file, motherCategory }) => {
     fd.append('motherCategory', motherCategory);
     fd.append('file', file);
 
-    return fetch(`${BASE_URL}/documents`, {
+    return apiFetch('/documents', {
       method: 'POST',
-      headers: getAuthHeaders(),
       body: fd,
-    })
-      .then(checkAuthResponse)
-      .then(handleHttpErrors)
-      .then(res => res.json())
+    }).then(res => res.json())
 
   } else {
-    return fetch(`${BASE_URL}/documents`, {
+    return apiFetch('/documents', {
       method: 'POST',
       body: JSON.stringify({ title, uri, motherCategory }),
-      headers: getAuthHeaders({
+      headers: {
         'Content-Type': 'application/json'
-      })
-    })
-      .then(checkAuthResponse)
-      .then(handleHttpErrors)
-      .then(res => res.json())
+      }
+    }).then(res => res.json())
   }
 };
 
-export const getTopLevelCategories = () => fetch(`${BASE_URL}/categories`).then(handleHttpErrors).then(res => res.json());
-export const getCategory = id => fetch(`${BASE_URL}/categories/${id}`).then(handleHttpErrors).then(res => res.json());
-export const getCategoryContent = id => fetch(`${BASE_URL}/categories/${id}?content=all`).then(handleHttpErrors).then(res => res.json());
-export const addCategory = (title, parentId) => fetch(`${BASE_URL}/categories`, {
+export const getTopLevelCategories = () => apiFetch('/categories').then(handleHttpErrors).then(res => res.json());
+
+export const getCategory = id => apiFetch(`/categories/${id}`).then(res => res.json());
+
+export const getCategoryContent = id => apiFetch(`/categories/${id}?content=all`).then(res => res.json());
+
+export const addCategory = (title, parentId) => apiFetch('/categories', {
   method: 'POST',
   body: JSON.stringify({ name: title, motherCategory: parentId }),
-  headers: getAuthHeaders({
+  headers: {
     'Content-Type': 'application/json'
-  })
-}).then(checkAuthResponse).then(handleHttpErrors).then(res => res.json());
-export const getCategories = () => fetch(`${BASE_URL}/categories`).then(handleHttpErrors).then(res => res.json());
+  }
+}).then(res => res.json());
 
-export const getUsers = () => fetch(`${BASE_URL}/users`).then(handleHttpErrors).then(res => res.json());
+export const getCategories = () => apiFetch('/categories').then(res => res.json());
 
-export const getUserDocs = () => fetch(BASE_URL + '/documents').then(handleHttpErrors).then(res => res.json());
+export const getUsers = () => apiFetch('/users').then(res => res.json());
 
-export const search = (query, types) => fetch(BASE_URL + '/search?q=' + encodeURIComponent(query)).then(res => res.json());
+export const getUserDocs = () => apiFetch('/users/me/documents').then(res => res.json());
 
-export const whoami = () => fetch(`${BASE_URL}/users/who`).then(checkAuthResponse);
+export const search = (query, types) => apiFetch('/search?q=' + encodeURIComponent(query)).then(res => res.json());
+
+export const whoami = () => apiFetch('/users/who');
